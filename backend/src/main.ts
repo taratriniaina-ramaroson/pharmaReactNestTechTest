1db3062e8d32995cffbec24bf6ce3ef3cc8577e4
RAMAROSON Taratriniaina <taratriniaina.ramaroson@gmail.com>
2026-05-17 20:17:40 +0300

    Add global exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors()
+  
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
